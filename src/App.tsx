20d7816668321d5c082f5839874f4e9a25014d99
Bruno Mathias <bm.grey@outlook.com>
2023-10-05 11:05:47 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,99 @@
+import { Button } from "./components/ui/button";
+import { Github, Wand2 } from "lucide-react";
+import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
+import { Label } from "./components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./components/ui/select";
+import { Slider } from "./components/ui/slider";
+import { VideoInputForm } from "./components/video-input-form";
+
+export function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-4 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">UploadVideo.IA</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido por Bruno Mathias
+          </span>
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant={"outline"}>
+            <Github className="w-4 h-4 mr-2" />
+            Github Project
+          </Button>
+        </div>
+      </div>
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              className="resize-none p-5 leading-relaxed"
+              placeholder="Inclua o Prompt do video aqui"
+            />
+            <Textarea
+              className="resize-none"
+              placeholder="Resultado gerado pela IA...."
+              readOnly
+            />
+          </div>
+          <p>
+            {" "}
+            Lembre-se, você pode utilizar a variável{" "}
+            <code className="text-violet-400">{"{transcription}"}</code> para
+            adicionar o conteúdo da transcrição do video{" "}
+          </p>
+        </div>
+
+        <aside className="w-80 space-y-6">
+
+        <VideoInputForm />
+
+          <Separator />
+
+          <form className="space-y-6 w-full">
+            <div className="space-y-2">
+              <Label>Modelo</Label>
+              <Select disabled defaultValue="gpt3.5">
+                <SelectTrigger className="w-full">
+                  <SelectValue></SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="gpt3.5">GPT 3.5-Turbo 16K</SelectItem>
+                </SelectContent>
+              </Select>
+
+              <span className="block text-sm text-muted-foreground italic">
+                Você poderá customizar esta opção em breve
+              </span>
+            </div>
+
+            <Separator />
+            <div className="space-y-4">
+              <Label>Temperatura</Label>
+              <Slider min={0} max={1} step={0.1} />
+
+              <span className="block text-sm text-muted-foreground italic">
+                Volores mais altos tendem deixar o resultado mais criativo,
+                porém menos preciso.
+              </span>
+            </div>
+
+            <Separator />
+
+            <Button type="submit" className="w-full">
+              Executar <Wand2 className="w-4 h-4 ml-2" />
+            </Button>
+          </form>
+        </aside>
+      </main>
+    </div>
+  );
+}
---
+++ src/components/video-input-form.tsx
@@ -0,0 +1,68 @@
+import { FileVideo, Upload } from "lucide-react";
+import { Separator } from "./ui/separator";
+import { Label } from "./ui/label";
+import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
+import { ChangeEvent, useState } from "react";
+
+export function VideoInputForm() {
+
+    const [videoFile, setVideoFile] = useState<File | null>(null)
+
+    function handleFileSelece(event: ChangeEvent<HTMLInputElement>){
+
+        const { files } = event.currentTarget
+        
+        if(!files) {
+            return
+        }
+
+        const selectecFile = files[0]
+
+        setVideoFile(selectecFile)
+    }
+
+    
+
+    return (
+        <form className="space-y-6 w-full">
+        <label
+          htmlFor="video"
+          className="border flex rounded-md aspect-video cursor-poiter border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/10"
+        >
+          {videoFile ? 'Video Selecionado' : (
+            <>  
+            <FileVideo className="w-4 h-4" />
+            Selecione um Video
+            </>
+          )}
+        </label>
+
+        <input
+          type="file"
+          id="video"
+          accept="video/mp4"
+          className="sr-only"
+          onChange={handleFileSelece}
+        />
+
+        <Separator />
+
+        <div className="space-y-1">
+          <Label htmlFor="transcription_prompt">
+            Prompt de Transcrição
+          </Label>
+          <Textarea
+            id="transcription_prompt"
+            className="h-28 leading-relaxed resize-none"
+            placeholder="Inclua palavras chaves mencionadas no video separadas por vírgula"
+          ></Textarea>
+        </div>
+
+        <Button type="submit" className="w-full">
+          Carregar Video
+          <Upload className="w-4 h-4 ml-2" />
+        </Button>
+      </form>
+    )
+}
